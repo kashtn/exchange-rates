7643e7fbb6360ae8551cb303061642340386f609
Ivan <ivan070797@yandex.ru>
2020-12-02 17:29:52 +0300

    deploy 17.29

--- myServer.js
+++ myServer.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
   console.log('123');
   // app.use(express.static(path.join(__dirname, "front/build")));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'front/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'front/build', 'manifest.json'));
   });
 }
 
